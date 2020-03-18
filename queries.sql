4c056b539bb542ef097ff48880915afbe1a1d606
qualmless <terasmash@gmail.com>
2020-03-17 19:29:42 -0500

    everything works!

--- queries.sql
+++ queries.sql
@@ -1,5 +1,9 @@
 ## Part 1: Test it with SQL
 SELECT * FROM job;
+SELECT * FROM skill;
+SELECT * FROM employer;
+SELECT * FROM job_skills;
+
 
 ## Part 2: Test it with SQL
 SELECT * FROM employer WHERE location="St. Louis";
@@ -8,3 +12,6 @@ SELECT * FROM employer WHERE location="St. Louis";
 DROP TABLE `techjobs`.`job`;
 
 ## Part 4: Test it with SQL
+SELECT skill.name,skill.description FROM skill 
+INNER JOIN job_skills ON skill.id = job_skills.skills_id
+ORDER BY skill.name;
